43fe3d6054b38cad25521ef7d0b9a75369c985a7
Luiz-F3lipe <luizf3lipe.dev@gmail.com>
2025-09-14 22:19:09 -0300

    create elements for LoginBottomSheetView

--- Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetViewController.swift
+++ Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetViewController.swift
@@ -9,8 +9,21 @@ import Foundation
 import UIKit
 
 class LoginBottomSheetViewController: UIViewController {
+    let loginView = LoginBottomSheetView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        self.view.addSubview(loginView)
     }
 }
